d12d7ffae593c55d23a2573e4d8f510697c8f1a1
winnye souza <wiinnye@gmail.com>
2025-10-31 21:38:30 -0300

    ajuste no layout

--- expo-api-routes/src/app/index.jsx
+++ expo-api-routes/src/app/index.jsx
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_APIKEY,
   authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
   projectId: process.env.EXPO_PUBLIC_PROJECTID,
-  storageBucket:process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINSENDERID,
   appId: process.env.EXPO_PUBLIC_APPID
 };
@@ -14,20 +14,21 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 
-import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, Text, View } from 'react-native';
 
-export default function App(){
+
+export default function App() {
   const [nomes, setNomes] = useState([]);
 
-  useEffect(() =>{
-    const fetchData = async () =>{
+  useEffect(() => {
+    const fetchData = async () => {
       const nomesCollection = firebase.firestore().collection('Nomes');
       const snapshot = await nomesCollection.get();
-    
+
       const data = [];
-      snapshot.forEach((doc) =>{
-        data.push({id: doc.id, ...doc.data() });
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
       });
 
       setNomes(data);
@@ -36,45 +37,66 @@ export default function App(){
     fetchData();
   }, []);
 
-  return(
-    <View 
-      style={{
-      width:"100%", 
-      height:"100%",
-      backgroundColor: "#fce2e2ff",
-      justifyContent:'center',
-      alignItems:'center'}}>
-        <View
-      style={{
-      width:"100%", 
-      height:'20%',
-      backgroundColor: "#de4242ff",
-      justifyContent:'center',
-      alignItems:'center',
-      }}
-      >
-        <Text style={{fontSize:"25px"}}>Lista de Nomes:</Text>
-      </View>
-      
-      <FlatList 
-      data={nomes}
-      keyExtractor={(item) => item.id}
-      renderItem={({item}) => (
-        <View
-        style={{
-      width:"100%", 
-      height:'auto',
-      padding:"1rem",
-      margin:'1rem',
-      backgroundColor: "#de4242ff",
-      justifyContent:'center',
-      alignItems:'center',
 
-      text:hover{ background-color:"#dcde42ff"},
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#EEF0F2"
       }}>
-          <Text  style={{fontSize:"20px"}}>{item.Nome} {item.Sobrenome}</Text>
-        </View>
-      )}
+      <View
+        style={{
+          width: "100%",
+          height: '10%',
+          backgroundColor: "#007090",
+        }}
+      >
+        <Image
+          style={{ width: '100px', height: "95px", alignSelf: "flex-start", padding: "10px" }}
+          source={{ uri: 'https://th.bing.com/th/id/R.1422944bd0e2453d6a2588f7ee305e6c?rik=DCQBmrX16yn13A&riu=http%3a%2f%2fblogs.powercode.id%2fwp-content%2fuploads%2f2022%2f09%2ffirebase3.png&ehk=WNZyhzq%2bgco2pRc50QvTZSxVcXufW2%2f29KPZ5Z6S26A%3d&risl=&pid=ImgRaw&r=0://cdn-icons-png.flaticon.com/512/53/53453.png' }}
+        />
+      </View>
+      <Text style={{
+        fontSize: "30px",
+        marginTop: "2rem",
+        marginBottom: "2rem",
+        marginLeft: ".9rem",
+        fontWeight: "500",
+
+      }}>Lista de Nomes:</Text>
+
+      <FlatList
+        data={nomes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              width: "100%",
+              padding: ".3rem",
+              marginBottom: '1rem',
+              justifyContent: 'center',
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                width: '50%',
+                height: "100%",
+                backgroundColor:"#629677",
+                padding: '.6rem'
+              }}
+            >
+              <Image
+                style={{ width: '40px', height: "40px", alignSelf: 'center' }}
+                source={{ uri: 'https://cdn-icons-png.flaticon.com/512/53/53453.png' }}
+              />
+              <Text style={{ fontSize: "20px", color: '#000', paddingLeft: ".5rem", alignSelf: 'center' }}>{item.Nome} {item.Sobrenome}</Text>
+            </div>
+          </View>
+        )}
+
       />
     </View>
   );
